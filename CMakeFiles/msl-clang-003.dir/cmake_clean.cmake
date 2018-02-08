cdb6524244b92935271e61282680250ad4d76203
Jeff <jeff.d.mcmillan@outlook.com>
2018-02-07 18:52:34 -0700

    Initial commit

---
+++ CMakeFiles/msl-clang-003.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msl-clang-003.dir/main.c.o"
+  "CMakeFiles/msl-clang-003.dir/mem_pool.c.o"
+  "CMakeFiles/msl-clang-003.dir/test_suite.c.o"
+  "msl-clang-003.pdb"
+  "msl-clang-003"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/msl-clang-003.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
