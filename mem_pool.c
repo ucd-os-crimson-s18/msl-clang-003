97167fb96793203368be92efe9dd280dc63c860e
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-02-09 15:56:26 -0700

    No changes

--- mem_pool.c
+++ mem_pool.c
@@ -418,7 +418,7 @@ alloc_status mem_del_alloc(pool_pt pool, void * alloc) {
         temp->next = NULL;
         //next->prev = NULL;
         temp->prev = NULL;
-        
+
         // this merged node-to-delete might need to be added to the gap index
         // but one more thing to check...
     }
@@ -427,12 +427,8 @@ alloc_status mem_del_alloc(pool_pt pool, void * alloc) {
     {
         //   remove the previous node from gap index
         //   check success
-        alloc_status result =
-                _mem_remove_from_gap_ix(pool_mgr, node->prev->alloc_record.size, node->prev);
-        if(result != ALLOC_OK)
-        {
-            return ALLOC_NOT_FREED;
-        }
+        _mem_remove_from_gap_ix(pool_mgr, node->prev->alloc_record.size, node->prev);
+
         //   add the size of node-to-delete to the previous
         node->prev->alloc_record.size += node->alloc_record.size;
         //   update node-to-delete as unused
@@ -440,20 +436,21 @@ alloc_status mem_del_alloc(pool_pt pool, void * alloc) {
         //   update metadata (used nodes)
         //pool_mgr->used_nodes -= 1;
 
+        node_pt temp = node->prev;
         //if (node_to_del->next) {
         if(node->next)
         {
             //prev->next = node_to_del->next;
-            node->prev->next = node->next;
+            temp->next = node->next;
             //node_to_del->next->prev = prev;
-            node->next->prev = node->prev;
+            node->next->prev = temp;
         }
         else
         {
             //prev->next = NULL;
-            node->prev->next = NULL;
+            temp->next = NULL;
         }
-        node_pt temp = node->prev;
+
         //node_to_del->next = NULL;
         node->next = NULL;
         //node_to_del->prev = NULL;
